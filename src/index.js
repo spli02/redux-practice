5277a2f713a9c417ddb3473c34d1cfa509f1ee34
yuri <yuri.haruta@gmail.com>
2019-10-04 19:10:30 +0200

    install redux

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { render } from 'react-dom'
+// import React from 'react'
+// import { render } from 'react-dom'
 
-import { User } from './components/User'
-import { Main } from './components/Main'
+// import { User } from './components/User'
+// import { Main } from './components/Main'
 
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      username: "Max"
-    }
-  }
+// class App extends React.Component{
+//   constructor(){
+//     super();
+//     this.state = {
+//       username: "Max"
+//     }
+//   }
 
-  changeUsername (newName) {
-    this.setState({
-      username: newName
-    });
-  }
+//   changeUsername (newName) {
+//     this.setState({
+//       username: newName
+//     });
+//   }
 
-  render(){
-    return(
-      <div className="container">
-        <Main changeUsername={this.changeUsername.bind(this)} />
-        <User username={this.state.username}/>
-      </div>
-    );
-  }
-}
+//   render(){
+//     return(
+//       <div className="container">
+//         <Main changeUsername={this.changeUsername.bind(this)} />
+//         <User username={this.state.username}/>
+//       </div>
+//     );
+//   }
+// }
 
-render(<App />, window.document.getElementById('app'))
+// render(<App />, window.document.getElementById('app'))
