34954cb1e3694460e1c3dfaf4b6f35642992cabd
yuri <yuri.haruta@gmail.com>
2019-10-05 21:27:01 +0200

    add basic store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import  { createStore } from "redux";
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+  }
+  return state;
+}
+const store = createStore(reducer,1);//1 is initial statement
+
+store.subscribe(() =>{
+  console.log("Store update!", store.getState())
+});
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+store.dispatch({
+  type: "ADD",
+  payload: 22
+});
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 80
+});
+
 // import React from 'react'
 // import { render } from 'react-dom'
 
